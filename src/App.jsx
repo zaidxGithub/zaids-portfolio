326a91b07cff7fa9f4caba227d38e3388a65d198
zaidxGithub <zaidxcodes@gmail.com>
2026-09-01 18:01:48 +0530

    Feat: Add QuickAnswerSection component to App

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import ExperienceSection from "./components/sections/ExperienceSection";
 import ContributionsSection from "./components/sections/ContributionsSection";
 import WinsSection from "./components/sections/WinsSection";
 import ContactSection from "./components/sections/ContactSection";
+import QuickAnswerSection from "./components/sections/QuickAnswersSection"
 
 export default function App() {
   const [theme, setTheme] = useState(() => localStorage.getItem("zaid-theme") || "dark");
@@ -46,6 +47,7 @@ export default function App() {
            <ExperienceSection/>
            <ContributionsSection/>
            <WinsSection/>
+           <QuickAnswerSection/>
            <ContactSection/>
         
 
